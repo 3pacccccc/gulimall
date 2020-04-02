76d806ff3119b5fc0dd749cf3e10113c68167b95
马锐敏 <351489917@qq.com>
2020-04-02 18:04:52 +0800

    23 12：35

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -1,4 +1,12 @@
 package com.atguigu.gulimall.member.feign;
 
-public class CouponFeignService {
+import com.atguigu.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
 }
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,31 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
+
+    @Autowired
+    private BrandService brandService;
 
     @Test
     void contextLoads() {
     }
 
+
+    @Test
+    public void brandServiceTest() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        boolean result = brandService.save(brandEntity);
+        System.out.println(result);
+    }
 }
