5581c4585e3c92383547293cde7da67d1d2c31d5
3pacccccc <351489917@qq.com>
2020-04-02 23:59:20 +0800

    入门over

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+// 由于引入了gulimall-common包，里面有mybatis依赖，所以需要配置数据库相关信息，exclude之后不需要
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
