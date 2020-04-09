f997dd781eb4bc15980800e683f8beb2a864d942
马锐敏 <351489917@qq.com>
2020-04-09 18:32:32 +0800

    90 03:33

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 1、整合MyBatis-Plus
@@ -55,7 +56,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
-@SpringBootApplication()
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.product.feign")
+@SpringBootApplication
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/BrandVo.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/BrandVo.java
@@ -5,8 +5,10 @@ import lombok.Data;
 @Data
 public class BrandVo {
 
+    /**
+     * "brandId": 0,
+     * "brandName": "string",
+     */
     private Long brandId;
-
-    private String brandName;
-
+    private String  brandName;
 }
