21af08542880d674c694125367edb5f9c79575a9
3pacccccc <351489917@qq.com>
2020-05-11 00:27:12 +0800

    190 OVER

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/vo/SearchResult.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/vo/SearchResult.java
@@ -18,12 +18,23 @@ public class SearchResult {
     private Integer pageNum;
     private Long total;
     private Integer totalPages;
+    private List<Integer> pageNavs;
 
     private List<BrandVo> brands; // 当前查询到的结果，所有涉及到的品牌
 
     private List<AttrVo> attrs; // 当前查询到的结果，所有涉及到的分类
 
     private List<CatalogVo> catalogs;// 当前查询到的结果，所有涉及到的属性
+
+    private List<NavVo> navs; // 面包屑导航数据
+
+    @Data
+    public static class NavVo {
+        private String navName;
+        private String navValue;
+        private String link;
+    }
+
 
     @Data
     public static class BrandVo {
