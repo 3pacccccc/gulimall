bc6a1d37ee98624e1e72f0fc213fa1f95e79d0e9
马锐敏 <351489917@qq.com>
2020-10-20 17:34:10 +0800

    撼地最后一次提交

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,16 +1,80 @@
 package com.atguigu.gulimall.product;
 
-import org.junit.Test;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
-    
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void brandServiceTest() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        boolean result = brandService.save(brandEntity);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testStringRedisTemplate() {
+        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
+
+        // 保存
+        stringStringValueOperations.set("hello", "world", 30, TimeUnit.SECONDS);
+
+        // 查询
+        String hello = stringStringValueOperations.get("hello");
+        System.out.println(hello);
+    }
+
+
+    @Test
+    public void uploadTest() {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+//        String accessKeyId = "*********************";
+//        String accessKeySecret = "*************************";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+//        // 创建PutObjectRequest对象。
+//        PutObjectRequest putObjectRequest = new PutObjectRequest("************", "2b1837c6c50add30.jpg", new File("*********"));
+//
+//        // 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+//        // ObjectMetadata metadata = new ObjectMetadata();
+//        // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+//        // metadata.setObjectAcl(CannedAccessControlList.Private);
+//        // putObjectRequest.setMetadata(metadata);
+//
+//        // 上传文件。
+//        ossClient.putObject(putObjectRequest);
+//
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+//
 
     }
 }
